1d4162ce034ee12d37fd8a4df8625b147a5bc82c
Felipe Román <ftroman@miuandes.cl>
2020-05-06 00:02:48 -0400

    Routes Prototype / Before controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,42 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "pages#home"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resource :user do  #user has many events and has one org role
+        resources :events, shallow: true
+        resource :organization_role, shallow: true
+      end
+      resources :organizations do  #orgs has many events and has many org roles
+        resources :events, :organization_roles, shallow: true
+      end
+      resource :user do  #user has many invitations, which are linked to comments and votes
+        resources :invitations, shallow: true do
+          resources :comments, :votes, shallow: true
+        end
+      end
+      resource :event do  #event has many invitations, comments and votes
+        resources :invitations, :comments, :votes, shallow: true
+      end
+    end
+    end
+  end
+
+  resource :user do  #user has many events and has one org role
+    resources :events, shallow: true
+    resource :organization_role, shallow: true
+  end
+  resources :organizations do  #orgs has many events and has many org roles
+    resources :events, :organization_roles, shallow: true
+  end
+  resource :user do  #user has many invitations, which are linked to comments and votes
+    resources :invitations, shallow: true do
+      resources :comments, :votes, shallow: true
+    end
+  end
+  resource :event do  #event has many invitations, comments and votes
+    resources :invitations, :comments, :votes, shallow: true
+    end
+  end
 end
