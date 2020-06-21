958250bff68a1f70096695d4a586be0b1741954d
Felipe Román <ftroman@miuandes.cl>
2020-06-21 19:31:54 -0400

    Fix in Event view | Logged users can see private events if they are invited

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,15 @@ class PagesController < ApplicationController
       @events << j
     end
 
+    if user_signed_in?
+      for j in Event.where(privacy: 1).order(date: :asc)
+        @guests = Invitation.where(event_id: j.id)
+        if @guests.pluck(:user_id).include? current_user.id
+          @events << j
+        end
+      end
+    end
+
   end
 end
 
