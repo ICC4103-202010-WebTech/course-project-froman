f1289cdb97b98ae5fd0e965bfb5dad076af14495
Felipe Román <ftroman@miuandes.cl>
2020-06-21 21:55:12 -0400

    Change password implemented | Delete user implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,23 +59,22 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      sign_in :user, @user, bypass: true
+      flash[:notice] = 'User was successfully updated.'
+      redirect_to @user
+    else
+      redirect_to @user
     end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    destroy_user_session_path
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Account was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
