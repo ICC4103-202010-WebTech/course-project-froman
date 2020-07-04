274564153d46035e48fea21ffe11e50fa2d04871
Felipe Román <ftroman@miuandes.cl>
2020-07-04 14:21:22 -0400

    Event search engine fixed | only by event name

--- app/controllers/search_events_controller.rb
+++ app/controllers/search_events_controller.rb
@@ -1,9 +1,45 @@
 class SearchEventsController < ApplicationController
   def search_events
-    @events = Event.where("name LIKE ? ",
-                        "%#{Event.sanitize_sql_like(params[:q])}%")
+    if admin_signed_in?
+      @events = Event.where("name LIKE ? ",
+                            "%#{Event.sanitize_sql_like(params[:q])}%")
 
-    logger.debug(render_to_string layout: "search_events")
-    render layout: "search_events"
+      logger.debug(render_to_string layout: "search_events")
+      render layout: "search_events"
+
+    elsif user_signed_in?
+      @all_events = Event.where("name LIKE ? ",
+                            "%#{Event.sanitize_sql_like(params[:q])}%")
+      @events = []
+      for j in @all_events
+        if j.privacy == 0
+          @events << j
+        end
+      end
+      for j in @all_events
+        if j.privacy == 1
+          @guests = Invitation.where(event_id: j.id)
+          if @guests.pluck(:user_id).include? current_user.id
+            @events << j
+          end
+        end
+      end
+
+      logger.debug(render_to_string layout: "search_events")
+      render layout: "search_events"
+
+    else
+      @all_events = Event.where("name LIKE ? ",
+                                "%#{Event.sanitize_sql_like(params[:q])}%")
+      @events = []
+      for j in @all_events
+        if j.privacy == 0
+          @events << j
+        end
+      end
+
+      logger.debug(render_to_string layout: "search_events")
+      render layout: "search_events"
+    end
   end
 end
